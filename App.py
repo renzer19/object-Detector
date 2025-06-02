703b749c736f1b789fd4f209c930e97d582382b0
renaldi <87884489+renzer19@users.noreply.github.com>
2025-06-02 23:34:50 +0700

    Update App.py

--- App.py
+++ App.py
@@ -2,10 +2,8 @@ import cv2
 import numpy as np
 import tensorflow as tf
 
-# Load model dari folder saved_model (pastikan pathnya bener ya)
 detect_fn = tf.saved_model.load("ssd_mobilenet_v2/saved_model")
 
-# Label hewan yang mau dideteksi dari COCO dataset
 category_index = {
     1: {'id': 1, 'name': 'person'},
     17: {'id': 17, 'name': 'cat'},
@@ -19,7 +17,6 @@ category_index = {
     25: {'id': 25, 'name': 'giraffe'},
 }
 
-# Buka webcam
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -27,21 +24,16 @@ while True:
     if not ret:
         break
 
-    # Convert frame ke tensor dengan dtype uint8 (penting!)
-    input_tensor = tf.convert_to_tensor([frame], dtype=tf.uint8)
-
-    # Jalankan deteksi
-    detections = detect_fn(input_tensor)
-
-    # Ambil hasil deteksi
-    num_detections = int(detections.pop('num_detections'))
-    detection_classes = detections['detection_classes'][0].numpy().astype(np.int32)
-    detection_scores = detections['detection_scores'][0].numpy()
-    detection_boxes = detections['detection_boxes'][0].numpy()
+    input_tensor    = tf.convert_to_tensor([frame], dtype=tf.uint8)
+    detections      = detect_fn(input_tensor)
+    
+    num_detections      = int(detections.pop('num_detections'))
+    detection_classes   = detections['detection_classes'][0].numpy().astype(np.int32)
+    detection_scores    = detections['detection_scores'][0].numpy()
+    detection_boxes     = detections['detection_boxes'][0].numpy()
 
     h, w, _ = frame.shape
 
-    # Loop tiap deteksi dan gambar bounding box
     for i in range(num_detections):
         if detection_scores[i] > 0.5:
             class_id = detection_classes[i]
@@ -55,10 +47,8 @@ while True:
                 cv2.putText(frame, label, (x1, y1 - 10),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
 
-    # Tampilkan frame hasil deteksi
     cv2.imshow("Animal Detector", frame)
 
-    # Tekan ESC untuk keluar
     if cv2.waitKey(1) & 0xFF == 27:
         break
 
